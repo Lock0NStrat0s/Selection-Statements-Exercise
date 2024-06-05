df920bf9880e3c3af90ba5272b1c239bdfb8ec55
Mohammad Jabir <mohammadj94@gmail.com>
2024-06-05 09:05:02 -0600

    completed exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1,1000);
+            int minNumber = 1;
+            int maxNumber = 1000;
+            int userInput = 0;
+
+            do 
+            {
+                Console.Write($"Enter a value between {minNumber} and {maxNumber}: ");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    minNumber = userInput;
+                    Console.WriteLine("Too Low!");
+                }
+                else if (userInput > favNumber)
+                {
+                    maxNumber = userInput;
+                    Console.WriteLine("Too High!");
+                }
+                else
+                {
+                    Console.WriteLine("You Win!");
+                }
+            } while (userInput != favNumber);
         }
     }
 }
